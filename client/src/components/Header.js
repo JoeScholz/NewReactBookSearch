d1edb66a1064bc07c4e74d5ca3ea5a8de40315a7
Joey2Jobs <scholz.joe@gmail.com>
2019-04-21 22:51:18 -0500

    rebuild repo

---
+++ client/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const headerStyle = {
+}
+
+function Header() {
+    return (
+        <header style={headerStyle}>
+            <h1>React Books Search</h1>
+            <p>Search and Save Books</p>
+        </header>
+    );
+}
+
+export default Header;
